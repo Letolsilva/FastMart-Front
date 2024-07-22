78cc45e649eda72be8b05e504bbb5bdc5c5364db
juliarezende34 <2019952135@teiacoltec.org>
2024-07-22 16:52:38 -0300

    add feature - login layout

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { TextInput } from "../../components/TextInput";
+
+const LoginSchema = yup.object().shape({
+    email: yup.string().required("E-mail é obrigatório"),
+    password: yup.string().required("Senha é obrigatória"),
+});
+
+export const Login: React.FC = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: LoginSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50">
+        <div className="max-w-6xl mx-auto p-8 bg-white border border-gray-300 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold mb-6 text-center text-primary">
+            Login
+          </h2>
+          <form onSubmit={formik.handleSubmit} className="grid grid-cols-1">
+            <div>  
+              <TextInput
+                title="Email"
+                placeholder="Digite seu email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                name="email"
+                className={
+                  formik.errors.email && formik.touched.email
+                    ? "border-red-500 w-96"
+                    : " w-96"
+                }
+              />
+              {formik.errors.email && formik.touched.email && (
+                <p className="text-red-500 text-xs ">
+                  {formik.errors.email}
+                </p>
+              )}
+  
+              <TextInput
+                title="Senha"
+                placeholder="Digite sua senha"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                name="password"
+                className={
+                  formik.errors.password && formik.touched.password
+                    ? "border-red-500 w-96"
+                    : "w-96"
+                }
+              />
+              {formik.errors.password && formik.touched.password && (
+                <p className="text-red-500 text-xs ">
+                  {formik.errors.password}
+                </p>
+              )}
+            </div>
+           
+            <button
+              type="submit"
+              className="col-span-2 w-full bg-primary text-white py-2 px-4 rounded-lg hover:bg-secondary transition-colors mt-14"
+            >
+              Entrar
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "../pages/HomePage";
 import MainPage from "../pages/MainPage";
 import { Register } from "../pages/Register";
+import { Login } from "../pages/Login";
 
 export const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/main" element={<MainPage />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
