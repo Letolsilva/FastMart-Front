5186473e6f8d31c01fdc3ed00ed7f64eeaac8f14
oilucaslima <lucaslimacefetdiv@gmail.com>
2024-07-23 15:06:09 -0300

    correção layout

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -6,19 +6,18 @@ const MainPage: React.FC = () => {
     <div className="flex flex-col min-h-screen bg-gray-200 bg-opacity-50">
       {/* Cabeçalho */}
       <Header />
-
       {/* Conteúdo Principal */}
-      <main className="flex p-12 items-center justify-center mt-11">
-        <ul className="grid grid-cols-3 gap-4 w-full">
-          <MenuMain nome = {"Cadastrar empresa"} local="./register"></MenuMain>
-          <MenuMain nome = {"Remover Empresa"} local="/"></MenuMain>
-          <MenuMain nome = {"Atualizar Empresa"} local="/"></MenuMain>
-          <MenuMain nome = {"Cadastrar Produto"} local="/"></MenuMain>
-          <MenuMain nome = {"Armazenamento do Produto"} local="/"></MenuMain>
-          <MenuMain nome = {"Atualizar Produto"} local="/"></MenuMain>
-          <MenuMain nome = {"Remover Produto"} local="/"></MenuMain>
-          <MenuMain nome = {"Registro Financeiro"} local="/"></MenuMain>
-          <MenuMain nome = {"Análise de Dados"} local="/"></MenuMain>
+      <main className="flex-grow flex items-center justify-center p-12">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full max-w-5xl">
+          <MenuMain nome={"Cadastrar empresa"} local="./register"></MenuMain>
+          <MenuMain nome={"Remover Empresa"} local="/"></MenuMain>
+          <MenuMain nome={"Atualizar Empresa"} local="/"></MenuMain>
+          <MenuMain nome={"Cadastrar Produto"} local="/"></MenuMain>
+          <MenuMain nome={"Armazenamento do Produto"} local="/"></MenuMain>
+          <MenuMain nome={"Atualizar Produto"} local="/"></MenuMain>
+          <MenuMain nome={"Remover Produto"} local="/"></MenuMain>
+          <MenuMain nome={"Registro Financeiro"} local="/"></MenuMain>
+          <MenuMain nome={"Análise de Dados"} local="/"></MenuMain>
         </ul>
       </main>
     </div>
