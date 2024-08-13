a98b63258c83bbba08e21a9d03b37c6aa464d8e1
juliarezende34 <2019952135@teiacoltec.org>
2024-08-13 14:50:48 -0300

    login update with react-auth-kit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,16 +2,55 @@ import React from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import { TextInput } from "../../components/TextInput";
-import { PostLogin } from "../../services/APIservices";
+import { toast } from "react-toastify";
+import { useSignIn } from "react-auth-kit";
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
+const API_URL = "http://localhost:3333";
 
 const LoginSchema = yup.object().shape({
-    email: yup.string().required("E-mail é obrigatório"),
-    password: yup.string().required("Senha é obrigatória"),
+  email: yup.string().required("E-mail é obrigatório"),
+  password: yup.string().required("Senha é obrigatória"),
 });
 
 export const Login: React.FC = () => {
-  const navigate = useNavigate();
+  const signIn = useSignIn();  // Hooks should be called at the top level
+  const navigate = useNavigate();  // Hooks should be called at the top level
+
+  const handleLogin = async (data: { email: string; password: string }) => {
+    try {
+      const response = await axios.post(`${API_URL}/users/login`, data);
+      if (response.status === 200) {
+        const { token } = response.data;
+        localStorage.setItem("authToken", token);
+        toast.success("Usuário logado com sucesso!");
+
+        signIn({
+          token: token,
+          expiresIn: 3600,
+          tokenType: "Bearer",
+          authState: { email: data.email },
+        });
+
+        navigate("/");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          if (error.response.status === 400) {
+            toast.error("E-mail ou senha incorretos!");
+          } else if (error.response.status === 403) {
+            toast.error("Usuário já logado!");
+          }
+        } else {
+          toast.error("Erro ao buscar dados da API");
+        }
+      }
+      throw error;
+    }
+  };
+
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -19,64 +58,58 @@ export const Login: React.FC = () => {
     },
     validationSchema: LoginSchema,
     onSubmit: async (values) => {
-      await PostLogin(values, navigate);
+      handleLogin({ email: values.email, password: values.password });
     },
   });
-  
-    return (
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50">
-        <div className="max-w-6xl mx-auto p-8 bg-white border border-gray-300 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-bold mb-6 text-center text-primary">
-            Login
-          </h2>
-          <form onSubmit={formik.handleSubmit} className="grid grid-cols-1">
-            <div>  
-              <TextInput
-                title="Email"
-                placeholder="Digite seu email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                name="email"
-                className={
-                  formik.errors.email && formik.touched.email
-                    ? "border-red-500 w-96"
-                    : " w-96"
-                }
-              />
-              {formik.errors.email && formik.touched.email && (
-                <p className="text-red-500 text-xs ">
-                  {formik.errors.email}
-                </p>
-              )}
-              <TextInput
-                title="Senha"
-                placeholder="Digite sua senha"
-                type="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                name="password"
-                className={
-                  formik.errors.password && formik.touched.password
-                    ? "border-red-500 w-96"
-                    : "w-96"
-                }
-              />
-              {formik.errors.password && formik.touched.password && (
-                <p className="text-red-500 text-xs ">
-                  {formik.errors.password}
-                </p>
-              )}
-            </div>
-              <button
-                type="submit"
-                className="col-span-2 w-full bg-primary text-white py-2 px-4 rounded-lg hover:bg-secondary transition-colors mt-14"
-              >
-                Entrar
-              </button>
-          </form>
-        </div>
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50">
+      <div className="max-w-6xl mx-auto p-8 bg-white border border-gray-300 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-6 text-center text-primary">Login</h2>
+        <form onSubmit={formik.handleSubmit} className="grid grid-cols-1">
+          <div>  
+            <TextInput
+              title="Email"
+              placeholder="Digite seu email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              name="email"
+              className={
+                formik.errors.email && formik.touched.email
+                  ? "border-red-500 w-96"
+                  : "w-96"
+              }
+            />
+            {formik.errors.email && formik.touched.email && (
+              <p className="text-red-500 text-xs">{formik.errors.email}</p>
+            )}
+            <TextInput
+              title="Senha"
+              placeholder="Digite sua senha"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              name="password"
+              className={
+                formik.errors.password && formik.touched.password
+                  ? "border-red-500 w-96"
+                  : "w-96"
+              }
+            />
+            {formik.errors.password && formik.touched.password && (
+              <p className="text-red-500 text-xs">{formik.errors.password}</p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="col-span-2 w-full bg-primary text-white py-2 px-4 rounded-lg hover:bg-secondary transition-colors mt-14"
+          >
+            Entrar
+          </button>
+        </form>
       </div>
-    );
-  };
+    </div>
+  );
+};
